e0b63480a2ac2f6ad9821cfae47c5dda3b4c2fc6
Roberto Ramos <56175688+rrfeisen@users.noreply.github.com>
2025-09-08 15:06:10 -0300

    Update arquivo_utils.h

--- tarefa01/arquivo_utils.h
+++ tarefa01/arquivo_utils.h
@@ -11,20 +11,20 @@ using namespace std;
  * @param nomeArquivo Nome do arquivo a ser lido
  * @return Vetor contendo os nomes lidos do arquivo
  */
-std::vector<std::string> lerNomesDoArquivo(const std::string& nomeArquivo);
+vector<string> lerNomesDoArquivo(const string& nomeArquivo);
 
 /**
  * Função para exibir os nomes na tela de forma formatada
  * @param nomes Vetor contendo os nomes a serem exibidos
  */
-void exibirNomes(const std::vector<std::string>& nomes);
+void exibirNomes(const vector<string>& nomes);
 
 /**
  * Função para salvar nomes em um arquivo de texto
  * @param nomes Vetor contendo os nomes a serem salvos
  * @param nomeArquivoSaida Nome do arquivo de saída
  */
-void salvarNomesEmArquivo(const std::vector<std::string>& nomes, 
-                         const std::string& nomeArquivoSaida);
+void salvarNomesEmArquivo(const vector<string>& nomes, 
+                         const stdstring& nomeArquivoSaida);
 
 #endif
